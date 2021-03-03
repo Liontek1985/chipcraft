121f49372537f45e0c1a4a107a967e6244751bc7
XUEFEI <awnuxcvbn@163.com>
2021-03-03 20:10:55 +0800

    file link

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ git apply ../0001-set-PocketCHIP-screen-resolution.patch
 make
 popd
 
-wget https://s3.amazonaws.com/assets.minecraft.net/pi/minecraft-pi-0.1.1.tar.gz
+wget https://github.com/xue-fei/chipcraft/raw/master/minecraft-pi-0.1.1.tar.gz
 tar xf minecraft-pi-0.1.1.tar.gz
 pushd mcpi
 cp ../mcrpi-wrapper/libbcm_host.so.1.0 libbcm_host.so
